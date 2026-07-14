f82135e1a271e8e59e545936e5b61a8156ad2a4e
Yasiru <yasiru@madhuwantha.com>
2026-07-14 15:34:13 +0530

    Added header section into the home component.

--- weather-app-react-basic/src/components/home/Home.jsx
+++ weather-app-react-basic/src/components/home/Home.jsx
@@ -1,7 +1,7 @@
 const Home = () => {
   return (
-    <div>
-      Home
+    <div className="flex flex-col items-center px-4 py-8">
+      <h1 className="text-2xl text-white font-semibold">Sri Lankan City Weather Report</h1>
     </div>
   );
 };
