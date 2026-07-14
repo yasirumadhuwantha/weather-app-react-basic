787d77d5cc7009ab2d730bd6b9e2567ae87d4672
Yasiru <yasiru@madhuwantha.com>
2026-07-14 10:27:42 +0530

    Create About component render part into react routing section.

--- weather-app-react-basic/src/App.jsx
+++ weather-app-react-basic/src/App.jsx
@@ -1,6 +1,7 @@
 import Navbar from "./components/common/Navbar.jsx";
 import { Route, Routes } from "react-router";
 import Home from "./components/Home/Home.jsx";
+import About from "./components/about/About.jsx";
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
 
       <Routes>
         <Route path="/" element={<Home/>}/>
+        <Route path="/" element={<About/>}/>
       </Routes>
     </div>
   );
