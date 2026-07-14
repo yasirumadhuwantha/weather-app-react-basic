0958d126f947808dfc781087736fba5b7c2cee09
Yasiru <yasiru@madhuwantha.com>
2026-07-14 14:53:15 +0530

    Added dummy weather data.

---
+++ weather-app-react-basic/src/components/data/weather.js
@@ -0,0 +1,26 @@
+export const WEATHER_DATA = {
+  anuradhapura: {
+    city: "Anuradhapura",
+    temperature: 31,
+    humidity: 78,
+    windSpeed: 14,
+  },
+  colombo: {
+    city: "Colombo",
+    temperature: 34,
+    humidity: 58,
+    windSpeed: 34,
+  },
+  kandy: {
+    city: "Kandy",
+    temperature: 21,
+    humidity: 88,
+    windSpeed: 54,
+  },
+  jaffna: {
+    city: "Jaffna",
+    temperature: 35,
+    humidity: 78,
+    windSpeed: 14,
+  },
+};
