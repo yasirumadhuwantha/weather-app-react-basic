7921b19e1622cfd64c0aca8932f998e080c522b8
Yasiru <yasiru@madhuwantha.com>
2026-07-14 09:41:36 +0530

    Installed Tailwind CSS & Setup.

--- weather-app-react-basic/src/App.jsx
+++ weather-app-react-basic/src/App.jsx
@@ -1,10 +1,9 @@
 const App = () => {
-  const [count, setCount] = useState(0)
 
   return (
-    <h1>
-      Hello
-    </h1>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
   )
 }
 
